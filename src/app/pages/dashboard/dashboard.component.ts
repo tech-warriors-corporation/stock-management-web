66d706c2ee105ce4121f593ff107014a1083594d
hotequil <hotequil@protonmail.com>
2022-09-11 18:55:35 -0300

    feat: init Dashboard page

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-dashboard',
+    template: '<app-layout><!-- TODO: dashboard --></app-layout>',
+})
+export class DashboardComponent {}
