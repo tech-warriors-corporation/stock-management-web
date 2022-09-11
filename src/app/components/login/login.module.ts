8cdfde91cdbfc0d909618c345a0179c4a968d2fc
hotequil <hotequil@protonmail.com>
2022-09-11 18:22:45 -0300

    feat: added developed-by no login

--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from "./login-routing.module";
+import { DevelopedByModule } from "../../shared/developed-by/developed-by.module";
 
 @NgModule({
     declarations: [
@@ -10,7 +11,8 @@ import { LoginRoutingModule } from "./login-routing.module";
     ],
     imports: [
         CommonModule,
-        LoginRoutingModule
+        LoginRoutingModule,
+        DevelopedByModule
     ],
 })
 export class LoginModule{}
