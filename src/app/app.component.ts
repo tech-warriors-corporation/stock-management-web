6d7433826cec5ef3192bb1ccac892a3de9415cbf
hotequil <hotequil@protonmail.com>
2022-09-13 21:22:46 -0300

    feat: imported full-loading component in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: '<div class="app"><router-outlet></router-outlet></div>'
+    template: `
+        <div class="app">
+            <app-full-loading></app-full-loading>
+            <router-outlet></router-outlet>
+        </div>
+    `
 })
 export class AppComponent{}
