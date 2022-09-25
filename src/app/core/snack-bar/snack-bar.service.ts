b44f4b6a638da3c49f161e5f187d709bc2197bdd
hotequil <hotequil@protonmail.com>
2022-09-25 20:00:57 -0300

    feat: done snack-bar

---
+++ src/app/core/snack-bar/snack-bar.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SnackBarService {
+    constructor(private snackBar: MatSnackBar){}
+
+    open(message: string): void{
+        this.snackBar.open(message, 'Fechar', {
+            duration: 8000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top'
+        });
+    }
+}
