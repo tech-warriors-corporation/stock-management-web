8984343dd6a3200753f93112b344eabbfacb9f56
hotequil <hotequil@protonmail.com>
2022-09-25 19:42:02 -0300

    feat: set a suffix in API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Env } from "../app/shared/types/env";
 
 export const environment: Env = {
     production: true,
-    api: 'http://127.0.0.1:5000' // TODO: put a production url here
+    api: 'http://127.0.0.1:5000/api' // TODO: put a production url here
 };
