bc7376079290f39a71f4d09a62dec2bfb6fc64f9
hotequil <hotequil@protonmail.com>
2022-09-25 13:28:33 -0300

    feat: added BrowserAnimationsModule in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { FullLoadingModule } from "./core/full-loading/full-loading.module";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        FullLoadingModule
+        FullLoadingModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
