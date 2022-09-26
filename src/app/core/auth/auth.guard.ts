c95aa7d8044ce0192a0d84d38810302c2b01122a
hotequil <hotequil@protonmail.com>
2022-09-25 21:49:58 -0300

    feat: done validations in auth guard

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -3,25 +3,41 @@ import { CanLoad, Route, Router } from '@angular/router';
 
 import { Path } from 'src/app/shared/enums/path';
 import { AuthService } from "./auth.service";
+import { FullLoadingService } from "../full-loading/full-loading.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
-    constructor(private authService: AuthService, private router: Router){}
+    constructor(private authService: AuthService, private router: Router, private fullLoadingService: FullLoadingService){}
 
-    canLoad(route: Route): boolean {
-        const isLogged = this.authService.isLogged;
+    async canLoad(route: Route): Promise<boolean> {
         const { path } = route;
+        const token = this.authService.token;
+        let isLogged = this.authService.isLogged;
+
+        if (!isLogged && token){
+            this.fullLoadingService.setShow(true);
+
+            try{
+                await this.authService.userByToken().toPromise()
+            } catch(error){
+                console.error(error)
+            } finally {
+                isLogged = this.authService.isLogged;
+
+                this.fullLoadingService.setShow(false);
+            }
+        }
 
         if (isLogged && path === Path.LOGIN){
-            this.router.navigateByUrl(`/${Path.DASHBOARD}`);
+            await this.router.navigateByUrl(`/${Path.DASHBOARD}`);
 
             return false;
         }
 
         if (!isLogged && path !== Path.LOGIN){
-            this.router.navigateByUrl(`/${Path.LOGIN}`);
+            await this.router.navigateByUrl(`/${Path.LOGIN}`);
 
             return false;
         }
