ebeb959e441e1f76f0be2289ea54f0af8981487f
hotequil <hotequil@protonmail.com>
2022-09-25 21:31:15 -0300

    feat: created an interceptor to set token in requests

---
+++ src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+import { Observable } from "rxjs";
+
+import { HeaderName } from "../../shared/enums/header-name";
+import { AuthService } from "../auth/auth.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TokenInterceptor implements HttpInterceptor{
+    constructor(private authService: AuthService){}
+
+    intercept (request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const token = this.authService.token;
+        let headers = new HttpHeaders({ 'Content-Type': 'application/json' })
+
+        if (token) headers = headers.append(HeaderName.TOKEN, token)
+
+        const clonedRequest = request.clone({ headers });
+
+        return next.handle(clonedRequest);
+    }
+}
