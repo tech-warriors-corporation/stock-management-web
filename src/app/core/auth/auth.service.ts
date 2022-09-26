9e72c80dc9afe6d5fcd12e9647d4fb959ae88381
hotequil <hotequil@protonmail.com>
2022-09-25 21:47:18 -0300

    feat: done login and userByToken request in auth service

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,16 +1,63 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+
+import { Observable, switchMap, tap } from "rxjs";
 
 import { User } from "../../shared/types/user";
+import { environment } from "../../../environments/environment";
+import { Response } from "../../shared/types/response";
+import { LocalStorage } from "../../shared/decorators/local-storage";
+import { StorageName } from "../../shared/enums/storage-name";
+import { Path } from "../../shared/enums/path";
+import { copy } from "../../shared/helpers/manipulate";
+
+type AuthUser = User | null;
+type AuthToken = string | null;
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    get user(): User | null {
-        return null
+    private loggedUser: AuthUser = null
+
+    @LocalStorage(StorageName.TOKEN)
+    private currentToken!: AuthToken;
+
+    constructor(private httpClient: HttpClient, private router: Router){}
+
+    get token(): AuthToken {
+        return copy(this.currentToken)
+    }
+
+    get user(): AuthUser {
+        return copy(this.loggedUser)
     }
 
     get isLogged(): boolean{
-        return !!this.user
+        return !!this.user?.userId
+    }
+
+    login({ email, userPassword }: { email: string, userPassword: string }): Observable<Response<AuthUser>>{
+        return this.httpClient
+                   .post<Response<AuthToken>>(`${environment.api}/login`, { email, userPassword })
+                   .pipe(
+                       switchMap(response => {
+                           this.currentToken = response.data
+
+                           return this.userByToken()
+                       })
+                   );
+    }
+
+    userByToken(): Observable<Response<AuthUser>>{
+        return this.httpClient.get<Response<AuthUser>>(`${environment.api}/user_by_token`).pipe(tap({
+            next: response => this.loggedUser = response.data,
+            error: () => {
+                this.loggedUser = null;
+                this.currentToken = null;
+                this.router.navigateByUrl(`/${Path.LOGIN}`);
+            }
+        }));
     }
 }
