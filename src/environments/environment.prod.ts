2a93f3bcef0994aacbd7d20973d1dec7dced5bd7
hotequil <hotequil@protonmail.com>
2022-09-26 22:27:39 -0300

    feat: added api production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Env } from "../app/shared/types/env";
 
 export const environment: Env = {
     production: true,
-    api: 'http://127.0.0.1:5000/api' // TODO: put a production url here
+    api: 'https://casulo-stock-management-api.herokuapp.com/api'
 };
