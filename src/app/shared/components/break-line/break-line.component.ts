545f1ed16b838953ee07aaa1e5c34554b1df8c4a
hotequil <hotequil@protonmail.com>
2022-09-28 20:15:55 -0300

    feat: done break-line component

---
+++ src/app/shared/components/break-line/break-line.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-break-line',
+    templateUrl: './break-line.component.html',
+    styleUrls: ['./break-line.component.scss']
+})
+export class BreakLineComponent{}
---
+++ src/app/shared/components/break-line/break-line.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BreakLineComponent } from './break-line.component';
+
+@NgModule({
+    declarations: [
+        BreakLineComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        BreakLineComponent
+    ],
+})
+export class BreakLineModule{}
