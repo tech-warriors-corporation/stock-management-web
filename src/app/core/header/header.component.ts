bd887cda63601defdb245979368e4239d27d5218
hotequil <hotequil@protonmail.com>
2022-09-27 23:10:16 -0300

    feat: made header principal functions

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { MatMenuTrigger } from "@angular/material/menu";
+
+import { Subscription } from "rxjs";
+
+import { AuthService } from "../auth/auth.service";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, OnDestroy{
+    @ViewChild(MatMenuTrigger) menuTrigger!: MatMenuTrigger;
+
+    title!: string
+    userName!: string|null
+
+    private data$!: Subscription
+
+    constructor(private authService: AuthService, private route: ActivatedRoute){}
+
+    ngOnInit(): void{
+        this.userName = this.authService.user?.userName || null
+        this.data$ = this.route.data.subscribe(({ title }) => this.title = title)
+    }
+
+    get isUserOptionsOpened(): boolean{
+        return !!this.menuTrigger?.menuOpen
+    }
+
+    logout(){
+        this.authService.logout()
+    }
+
+    ngOnDestroy(): void{
+        this.data$?.unsubscribe()
+    }
+}
--- src/app/core/header/header.module.ts
+++ src/app/core/header/header.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
 
 import { HeaderComponent } from "./header.component";
 
 @NgModule({
     declarations: [HeaderComponent],
     exports: [HeaderComponent],
-    imports: [CommonModule]
+    imports: [CommonModule, MatButtonModule, MatMenuModule, MatIconModule]
 })
 export class HeaderModule{}
