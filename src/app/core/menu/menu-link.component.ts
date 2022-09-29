1fef4f802dda7b32ab046b9b373f9dcaab2e1062
hotequil <hotequil@protonmail.com>
2022-09-28 21:55:41 -0300

    feat: created MenuLink component

---
+++ src/app/core/menu/menu-link.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-menu-link',
+    templateUrl: './menu-link.component.html',
+    styleUrls: ['./menu-link.component.scss']
+})
+export class MenuLinkComponent {
+    @Input() text!: string
+    @Input() href!: string
+    @Input() icon!: string
+}
---
+++ src/app/shared/types/menu-link.ts
@@ -0,0 +1,5 @@
+export type MenuLink = {
+    href: string
+    text: string
+    icon: string
+}
