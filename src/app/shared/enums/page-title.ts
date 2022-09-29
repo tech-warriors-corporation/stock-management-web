1a485aeec179782d3b39f30ee7902eaff1b463de
hotequil <hotequil@protonmail.com>
2022-09-28 21:28:31 -0300

    feat: created PageTitle enum

---
+++ src/app/shared/enums/page-title.ts
@@ -0,0 +1,9 @@
+export enum PageTitle{
+    DASHBOARD = 'Dashboard',
+    STOCK = 'Estoque',
+    INPUTS_AND_OUTPUTS = 'Entradas e saídas',
+    PRODUCTS = 'Produtos',
+    CATEGORIES = 'Categorias',
+    USERS = 'Usuários',
+    NOT_FOUND = 'Página não encontrada',
+}
