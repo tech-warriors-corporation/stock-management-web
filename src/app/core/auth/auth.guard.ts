d20578d92f1d754bc4983a213787daa041dc4fc8
hotequil <hotequil@protonmail.com>
2022-09-29 20:21:43 -0300

    feat: validated admin routes in AuthGuard

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -5,6 +5,8 @@ import { Path } from 'src/app/shared/enums/path';
 import { AuthService } from "./auth.service";
 import { FullLoadingService } from "../full-loading/full-loading.service";
 
+export const getAdminRoutes = () => [Path.USERS, Path.CATEGORIES, Path.PRODUCTS]
+
 @Injectable({
     providedIn: 'root'
 })
@@ -14,7 +16,9 @@ export class AuthGuard implements CanLoad {
     async canLoad(route: Route): Promise<boolean> {
         const { path } = route;
         const token = this.authService.token;
+        const adminRoutes = getAdminRoutes()
         let isLogged = this.authService.isLogged;
+        let isAdmin = this.authService.isAmin;
 
         if (!isLogged && token){
             this.fullLoadingService.setShow(true);
@@ -25,9 +29,16 @@ export class AuthGuard implements CanLoad {
                 console.error(error)
             } finally {
                 isLogged = this.authService.isLogged;
+                isAdmin = this.authService.isAmin;
 
                 this.fullLoadingService.setShow(false);
             }
+        }
+
+        if (isLogged && !isAdmin && adminRoutes.includes(path as Path)){
+            await this.router.navigateByUrl(`/${Path.DASHBOARD}`);
+
+            return false
         }
 
         if (isLogged && path === Path.LOGIN){
