5bbe9a9df00ae9d7e918da6f45fea617a6120d0c
hotequil <hotequil@protonmail.com>
2022-10-01 23:42:17 -0300

    feat: cleared mat field margin and added default pristine parameter in pipe

--- src/app/shared/pipes/form/form-invalid.pipe.ts
+++ src/app/shared/pipes/form/form-invalid.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'formInvalid'
 })
 export class FormInvalidPipe implements PipeTransform {
-    transform(invalid: boolean, pristine: boolean): boolean {
+    transform(invalid: boolean, pristine = false): boolean {
         return invalid || pristine;
     }
 }
