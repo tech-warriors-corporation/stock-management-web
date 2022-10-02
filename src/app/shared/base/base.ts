fcd09a06f2fbe2cbe3aa272c8aa6ea9e0d052e43
hotequil <hotequil@protonmail.com>
2022-10-02 14:59:00 -0300

    feat: improved base class

--- src/app/shared/base/base.ts
+++ src/app/shared/base/base.ts
@@ -1,7 +1,8 @@
 export abstract class Base<T> {
-    protected abstract count: number
-    protected abstract page: number
-    protected abstract get(): void
     abstract loading: boolean
     abstract list: T[]
+    abstract count: number
+    protected abstract page: number
+    protected readonly abstract perPage: number
+    protected abstract get(): void
 }
