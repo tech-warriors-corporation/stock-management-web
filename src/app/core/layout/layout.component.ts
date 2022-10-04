c8e67d1315715a89ea4d88413f3a0dbfcc9fec9f
hotequil <hotequil@protonmail.com>
2022-10-03 21:40:37 -0300

    feat: done verification in layout

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from "@angular/router";
+
+import { filter, Subscription } from "rxjs";
+
+import { Path } from "../../shared/enums/path";
 
 @Component({
     selector: 'app-layout',
     template: `
-        <div class="layout">
+        <div class="layout" *ngIf="!isOutOfApplication; else contentTemplate">
             <app-header class="layout__header"></app-header>
             <app-menu class="layout__menu">
-                <div class="layout__content"><ng-content></ng-content></div>
+                <div class="layout__content"><ng-container *ngTemplateOutlet="contentTemplate"></ng-container></div>
             </app-menu>
         </div>
+        
+        <ng-template #contentTemplate><ng-content></ng-content></ng-template>
     `,
     styleUrls: ['layout.component.scss']
 })
-export class LayoutComponent{}
+export class LayoutComponent implements OnDestroy, OnInit{
+    isOutOfApplication = false
+
+    private routesOutOfApplication = [Path.LOGIN]
+    private routerEvents$!: Subscription;
+
+    constructor(private router: Router){}
+
+    ngOnInit(): void{
+        this.routerEvents$ = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: any)  => {
+            this.isOutOfApplication = this.routesOutOfApplication.some(url => event.url.includes(url))
+        })
+    }
+
+    ngOnDestroy(): void{
+        this.routerEvents$?.unsubscribe();
+    }
+}
