3adf86e4dfdf899043ebabccbc1ef20c27379785
hotequil <hotequil@protonmail.com>
2022-10-04 21:08:34 -0300

    feat: created newItem method in UsersService

--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
-import { User, Users } from "../../shared/types/user";
+import { NewUser, User, Users } from "../../shared/types/user";
 import { Response } from "../../shared/types/response";
 import { environment } from "../../../environments/environment";
-import { API, DeleteItem, GetList } from "../../shared/interfaces/restful";
+import { API, DeleteItem, GetList, NewItem } from "../../shared/interfaces/restful";
 
 @Injectable({
     providedIn: 'root'
 })
-export class UsersService implements API, GetList<User>, DeleteItem{
+export class UsersService implements API, GetList<User>, DeleteItem, NewItem{
     readonly API = `${environment.api}/users`
 
     constructor(private httpClient: HttpClient){}
@@ -21,6 +21,10 @@ export class UsersService implements API, GetList<User>, DeleteItem{
         if (email) Object.assign(params, { email })
 
         return this.httpClient.get<Response<Users>>(this.API, { params })
+    }
+
+    newItem(user: NewUser){
+        return this.httpClient.post<Response<null>>(this.API, user);
     }
 
     deleteItem(id: number){
