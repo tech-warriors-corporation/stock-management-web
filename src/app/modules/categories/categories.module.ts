9c71dca296d99973343d1f4cf8e081d1934ab984
hotequil <hotequil@protonmail.com>
2022-10-08 10:33:48 -0300

    feat: done style and module for categories list

--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from "@angular/material/table";
 
 import { CategoriesRoutingModule } from "./categories-routing.module";
 import { CategoriesComponent } from "./categories.component";
 import { BuildingPageModule } from "../../shared/components/building-page/building-page.module";
+import { HighlightPageModule } from "../../shared/components/highlight-page/highlight-page.module";
+import { ButtonModule } from "../../shared/components/button/button.module";
+import { FiltersModule } from "../../shared/components/filters/filters.module";
+import { InputModule } from "../../shared/components/input/input.module";
+import { HasShowMoreModule } from "../../shared/pipes/has-show-more/has-show-more.module";
+import { ContentLoadingModule } from "../../shared/components/content-loading/content-loading.module";
+import { EmptyStateModule } from "../../shared/components/empty-state/empty-state.module";
 
 @NgModule({
     declarations: [CategoriesComponent],
     imports: [
         CommonModule,
         CategoriesRoutingModule,
-        BuildingPageModule
+        BuildingPageModule,
+        HighlightPageModule,
+        ButtonModule,
+        FiltersModule,
+        InputModule,
+        MatTableModule,
+        HasShowMoreModule,
+        ContentLoadingModule,
+        EmptyStateModule
     ]
 })
 export class CategoriesModule {}
