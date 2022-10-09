bedea12f4689b9b5ed7b64002ca33e10668a7b5b
hotequil <hotequil@protonmail.com>
2022-10-09 19:20:17 -0300

    feat: created editItem and getItem in categories service

--- src/app/modules/categories/categories.service.ts
+++ src/app/modules/categories/categories.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
+import { Observable } from "rxjs";
+
 import { Response } from "../../shared/types/response";
 import { environment } from "../../../environments/environment";
-import { API, DeleteItem, GetList, NewItem } from "../../shared/interfaces/restful";
-import { Categories, Category, NewCategory } from "../../shared/types/category";
+import { API, DeleteItem, EditItem, GetItem, GetList, NewItem } from "../../shared/interfaces/restful";
+import { Categories, Category, EditCategory, NewCategory } from "../../shared/types/category";
 
 @Injectable({
     providedIn: 'root'
 })
-export class CategoriesService implements API, GetList<Category>, DeleteItem, NewItem<NewCategory>{
+export class CategoriesService implements API, GetList<Category>, DeleteItem, NewItem<NewCategory>, GetItem<Category>, EditItem<EditCategory>{
     readonly API = `${environment.api}/categories`
 
     constructor(private httpClient: HttpClient){}
@@ -28,5 +30,13 @@ export class CategoriesService implements API, GetList<Category>, DeleteItem, Ne
 
     newItem(category: NewCategory){
         return this.httpClient.post<Response<null>>(this.API, category);
+    }
+
+    getItem(id: number): Observable<Response<Category>> {
+        return this.httpClient.get<Response<Category>>(`${this.API}/${id}`)
+    }
+
+    editItem(id: number, category: EditCategory): Observable<Response<null>> {
+        return this.httpClient.patch<Response<null>>(`${this.API}/${id}`, category)
     }
 }
