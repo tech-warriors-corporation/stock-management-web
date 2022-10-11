432248d7ce3e9e647c3ccf26776c5689c658262f
hotequil <hotequil@protonmail.com>
2022-10-11 19:40:03 -0300

    feat: created variable to productName maxlength

--- src/app/shared/components/form/form-constants.ts
+++ src/app/shared/components/form/form-constants.ts
@@ -2,6 +2,7 @@ export class FormConstants{
     static readonly USER_NAME_MAXLENGTH = 80;
     static readonly EMAIL_MAXLENGTH = 50;
     static readonly CATEGORY_NAME_MAXLENGTH = 50;
+    static readonly PRODUCT_NAME_MAXLENGTH = 50;
     static readonly PASSWORD_MAXLENGTH = 80;
     static readonly PASSWORD_MINLENGTH = 8;
 }
