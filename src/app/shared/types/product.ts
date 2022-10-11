494a83ceedd7d3744b5b99bc00177e9185d55f0e
hotequil <hotequil@protonmail.com>
2022-10-11 19:43:27 -0300

    feat: created types for list and item Product

---
+++ src/app/shared/types/product.ts
@@ -0,0 +1,16 @@
+import { BooleanAsNumber } from "../enums/boolean-as-number";
+
+export type Product = {
+    productId: number
+    categoryId: number
+    productName: string
+    quantity: number
+    createdByUserId: number
+    isActive: BooleanAsNumber
+    dtCreated: Date
+    categoryName: string
+    categoryIsActive: BooleanAsNumber
+    dtUpdated?: Date
+}
+
+export type Products = Product[]
