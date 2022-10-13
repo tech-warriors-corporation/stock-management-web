cdc9e43305329abded2a9eabbeb67665e36ba0cf
hotequil <hotequil@protonmail.com>
2022-10-12 21:51:42 -0300

    feat: added getItem and editItem in ProductsService

--- src/app/modules/products/products.service.ts
+++ src/app/modules/products/products.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
+import { Observable } from "rxjs";
+
 import { Response } from "../../shared/types/response";
 import { environment } from "../../../environments/environment";
-import { API, DeleteItem, GetList, NewItem } from "../../shared/interfaces/restful";
-import { NewProduct, Product, Products } from "../../shared/types/product";
+import { API, DeleteItem, EditItem, GetItem, GetList, NewItem } from "../../shared/interfaces/restful";
+import { EditProduct, NewProduct, Product, Products } from "../../shared/types/product";
 
 @Injectable({
     providedIn: 'root'
 })
-export class ProductsService implements API, GetList<Product>, DeleteItem, NewItem<NewProduct>{
+export class ProductsService implements API, GetList<Product>, DeleteItem, NewItem<NewProduct>, GetItem<Product>, EditItem<EditProduct>{
     readonly API = `${environment.api}/products`
 
     constructor(private httpClient: HttpClient){}
@@ -29,5 +31,13 @@ export class ProductsService implements API, GetList<Product>, DeleteItem, NewIt
 
     newItem(product: NewProduct){
         return this.httpClient.post<Response<null>>(this.API, product);
+    }
+
+    getItem(id: number): Observable<Response<Product>> {
+        return this.httpClient.get<Response<Product>>(`${this.API}/${id}`)
+    }
+
+    editItem(id: number, product: EditProduct): Observable<Response<null>> {
+        return this.httpClient.patch<Response<null>>(`${this.API}/${id}`, product)
     }
 }
