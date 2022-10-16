a65a1adee876772b2424d9f8d74c295d7cb3fa34
hotequil <hotequil@protonmail.com>
2022-10-16 13:30:16 -0300

    feat: used text prop in support dialog

--- src/app/core/support/support.component.ts
+++ src/app/core/support/support.component.ts
@@ -6,15 +6,16 @@ import { DialogService } from "../dialog/dialog.service";
 @Component({
     selector: 'app-support',
     template: `
-        <app-dialog primaryTitle="Suporte" secondaryTitle="Como entrar em contato">
-            <p>
-                O sistema é mantido voluntariamente por desenvolvedores que estudaram 
-                na FIAP, essa aplicação foi feita com intuito de ajudar o Projeto Casulo, 
-                estamos dispostos a fazer melhorias e corrigir possíveis problemas 
-                encontrados por vocês, se precisarem entrar em contato conosco, enviem 
-                um e-mail para <a href="mailto:joaohotequil@gmail.com">joaohotequil@gmail.com</a> (desenvolvedor full stack e designer) e 
-                para <a href="mailto:lucasng2002@gmail.com">lucasng2002@gmail.com</a> (gerente do projeto), com isso, aguarde nossa resposta, obrigado!
-            </p>
+        <app-dialog primaryTitle="Suporte"
+                    secondaryTitle="Como entrar em contato"
+                    text='
+                        O sistema é mantido voluntariamente por desenvolvedores que estudaram 
+                        na FIAP, essa aplicação foi feita com intuito de ajudar o Projeto Casulo, 
+                        estamos dispostos a fazer melhorias e corrigir possíveis problemas 
+                        encontrados por vocês, se precisarem entrar em contato conosco, enviem 
+                        um e-mail para <a href="mailto:joaohotequil@gmail.com">joaohotequil@gmail.com</a> (desenvolvedor full stack e designer) e
+                        para <a href="mailto:lucasng2002@gmail.com">lucasng2002@gmail.com</a> (gerente do projeto), com isso, aguardem nossa resposta, obrigado!
+                    '>
             <app-button [layout]="buttonLayout.FLAT"
                         (inClick)="close()"
                         dialog-action
