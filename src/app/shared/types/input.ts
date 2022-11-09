f5cfd1514e4ee55a41e20140e2ddd98fd5373130
hotequil <hotequil@protonmail.com>
2022-11-08 22:02:55 -0300

    feat: created Input type

---
+++ src/app/shared/types/input.ts
@@ -0,0 +1,23 @@
+import { Product } from "./product";
+import { BooleanAsNumber } from "../enums/boolean-as-number";
+import { User } from "./user";
+import { Category } from "./category";
+
+export type Input = {
+    inputId: number
+    product: Product
+    category: Category
+    productQuantity: number
+    hasProductExpiration: BooleanAsNumber
+    isDonation: BooleanAsNumber
+    isActive: BooleanAsNumber
+    createdBy: User
+    dtEntered: Date
+    dtCreated: Date
+    inputDescription: string | null
+    unitPrice: number | null
+    totalPrice: number | null
+    dtUpdated: Date | null
+}
+
+export type Inputs = Input[]
