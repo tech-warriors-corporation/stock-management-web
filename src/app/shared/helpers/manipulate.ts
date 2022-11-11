0dfc8dd5a7d958f895c721c6532ff4ce74d2dd4a
hotequil <hotequil@protonmail.com>
2022-11-11 20:02:27 -0300

    feat: created createResponse method

--- src/app/shared/helpers/manipulate.ts
+++ src/app/shared/helpers/manipulate.ts
@@ -1,2 +1,5 @@
+import { Response } from "../types/response";
+
 export const NONE_VALUE = ''
 export const copy = (value: any) => JSON.parse(JSON.stringify(value))
+export const createResponse = <T = any>(data: T, count: number | null = null): Response<T> => ({ data, count })
