f25233530e331e86fea36716858d76c3a9b2f1ea
hotequil <hotequil@protonmail.com>
2022-11-11 21:32:54 -0300

    feat: added createdById in InputOutputFilter

--- src/app/shared/types/input-output.ts
+++ src/app/shared/types/input-output.ts
@@ -3,4 +3,5 @@ import { BooleanAsNumber } from "../enums/boolean-as-number";
 export type InputOutputFilter = {
     productId: number | null
     hasProductExpiration: BooleanAsNumber | null
+    createdById: number | null
 }
