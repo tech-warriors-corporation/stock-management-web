76a367af3ab1efa8ebd04c564b435dd952569f89
hotequil <hotequil@protonmail.com>
2022-11-14 19:42:34 -0300

    feat: added prefix in uniqueId method

--- src/app/shared/helpers/unique-id.ts
+++ src/app/shared/helpers/unique-id.ts
@@ -1,1 +1,1 @@
-export const generateUniqueId = (): string => Math.floor(Math.random() * Date.now()).toString();
+export const generateUniqueId = (prefix = 'id'): string => `${prefix}${Math.floor(Math.random() * Date.now())}`;
