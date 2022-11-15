545514d2ff73bee0b39b8ca5e0bd1c764c694eed
hotequil <hotequil@protonmail.com>
2022-11-15 19:09:09 -0300

    feat: added more modules in inputs-outputs

--- src/app/modules/inputs-outputs/inputs-outputs.module.ts
+++ src/app/modules/inputs-outputs/inputs-outputs.module.ts
@@ -4,13 +4,30 @@ import { CommonModule } from '@angular/common';
 import { InputsOutputsRoutingModule } from "./inputs-outputs-routing.module";
 import { InputsOutputsComponent } from "./inputs-outputs.component";
 import { BuildingPageModule } from "../../shared/components/building-page/building-page.module";
+import { InputsModule } from "../inputs/inputs.module";
+import { HighlightPageModule } from "../../shared/components/highlight-page/highlight-page.module";
+import { ButtonModule } from "../../shared/components/button/button.module";
+import { FiltersModule } from "../../shared/components/filters/filters.module";
+import { AutocompleteModule } from "../../shared/components/autocomplete/autocomplete.module";
+import { InputsOutputsDescriptionComponent } from "./description/inputs-outputs-description.component";
+import { DialogModule } from "../../core/dialog/dialog.module";
+import { SelectModule } from "../../shared/components/select/select.module";
+import { DatePickerModule } from "../../shared/components/date-picker/date-picker.module";
 
 @NgModule({
-    declarations: [InputsOutputsComponent],
+    declarations: [InputsOutputsComponent, InputsOutputsDescriptionComponent],
     imports: [
         CommonModule,
         InputsOutputsRoutingModule,
-        BuildingPageModule
+        BuildingPageModule,
+        InputsModule,
+        HighlightPageModule,
+        ButtonModule,
+        FiltersModule,
+        AutocompleteModule,
+        DialogModule,
+        SelectModule,
+        DatePickerModule,
     ]
 })
 export class InputsOutputsModule {}
