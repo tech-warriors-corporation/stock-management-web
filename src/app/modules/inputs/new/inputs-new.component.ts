72199c331e620e1da42d765b946a080846551811
hotequil <hotequil@protonmail.com>
2022-11-15 18:24:39 -0300

    feat: init InputsNew component

---
+++ src/app/modules/inputs/new/inputs-new.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-inputs-new',
+    template: '<app-building-page></app-building-page>',
+})
+export class InputsNewComponent{}
