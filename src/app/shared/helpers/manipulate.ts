99392234267eed4be6ddaebb407527eeb7a6e8cd
hotequil <hotequil@protonmail.com>
2022-11-17 21:56:29 -0300

    feat: created unsubscribeForAll method

--- src/app/shared/helpers/manipulate.ts
+++ src/app/shared/helpers/manipulate.ts
@@ -1,5 +1,8 @@
+import { Subscription } from "rxjs";
+
 import { Response } from "../types/response";
 
 export const NONE_VALUE = ''
 export const copy = (value: any) => JSON.parse(JSON.stringify(value))
 export const createResponse = <T = any>(data: T, count: number | null = null): Response<T> => ({ data, count })
+export const unsubscribeForAll = (subscriptions: Subscription[]) => subscriptions.forEach(subscription => subscription.unsubscribe())
