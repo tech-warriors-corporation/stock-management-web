03146f1ed1fabffbe4a43c8b4916f7f6d28daf5e
hotequil <hotequil@protonmail.com>
2022-12-06 19:22:08 -0300

    feat(dashboard): init Dashboard component

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,7 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+
+import { finalize } from "rxjs";
+
+import { DashboardCardFilter } from "../../shared/types/dashboard-card-filter";
+import { InputsService } from "../inputs/inputs.service";
+import { SnackBarService } from "../../core/snack-bar/snack-bar.service";
 
 @Component({
     selector: 'app-dashboard',
-    template: '<app-building-page></app-building-page>',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {}
+export class DashboardComponent implements AfterViewInit{
+    productsDonatedAmount = 0
+    productsDonatedLoading = false
+
+    constructor(
+        private inputsService: InputsService,
+        private snackBarService: SnackBarService,
+        private changeDetector: ChangeDetectorRef,
+    ){}
+
+    ngAfterViewInit(){
+        this.changeDetector.detectChanges()
+    }
+
+    getProductsDonated(filters: DashboardCardFilter){
+        this.productsDonatedLoading = true
+
+        this.inputsService
+            .getProductsDonated(filters)
+            .pipe(finalize(() => this.productsDonatedLoading = false))
+            .subscribe(
+                ({ count }) => this.productsDonatedAmount = count as number,
+                () => this.snackBarService.open('Ocorreu um problema ao obter a quantidade de produtos doados nas entradas'),
+            )
+    }
+}
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,13 +4,24 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from "./dashboard-routing.module";
 import { DashboardComponent } from "./dashboard.component";
 import { BuildingPageModule } from "../../shared/components/building-page/building-page.module";
+import { DashboardCardComponent } from "./dashboard-card/dashboard-card.component";
+import { HighlightPageModule } from "../../shared/components/highlight-page/highlight-page.module";
+import { FiltersModule } from "../../shared/components/filters/filters.module";
+import { DatePickerModule } from "../../shared/components/date-picker/date-picker.module";
+import { ContentLoadingModule } from "../../shared/components/content-loading/content-loading.module";
+import { PluralizeModule } from "../../shared/pipes/pluralize/pluralize.module";
 
 @NgModule({
-    declarations: [DashboardComponent],
+    declarations: [DashboardComponent, DashboardCardComponent],
     imports: [
         CommonModule,
         DashboardRoutingModule,
-        BuildingPageModule
+        BuildingPageModule,
+        HighlightPageModule,
+        FiltersModule,
+        DatePickerModule,
+        ContentLoadingModule,
+        PluralizeModule,
     ]
 })
 export class DashboardModule{}
