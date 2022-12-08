4bf714c5b459bc7067d7e231bfa943f445228dfe
hotequil <hotequil@protonmail.com>
2022-12-08 20:34:47 -0300

    feat: create MonthAbbreviation enum

---
+++ src/app/shared/enums/month-abbreviation.ts
@@ -0,0 +1,14 @@
+export enum MonthAbbreviation{
+    JANUARY = 'Jan.',
+    FEBRUARY = 'Fev.',
+    MARCH = 'Mar.',
+    APRIL = 'Abr.',
+    MAY = 'Mai.',
+    JUNE = 'Jun.',
+    JULY = 'Jul.',
+    AUGUST = 'Ago.',
+    SEPTEMBER = 'Set.',
+    OCTOBER = 'Out.',
+    NOVEMBER = 'Nov.',
+    DECEMBER = 'Dez.',
+}
